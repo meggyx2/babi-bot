190fd41ce8b4568222c4e250a100ea54f60d6bfd
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-01-04 20:34:50 +0200

    Create cv.py

---
+++ cv.py
@@ -0,0 +1,6 @@
+import discord
+from discord import Message, Guild, Member
+from typing import Optional
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="*", case_insensitive=True)
