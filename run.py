1235fe075ef53444377610cb65ae543d313befc0
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-01-04 20:36:15 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,25 @@
+import discord
+from discord import Message, Guild, Member
+from typing import Optional
+from discord.ext import commands
 
+import os
+import sys
+import traceback
+import asyncio
+from datetime import datetime
+import datetime
+import random
+import json
+import requests
+
+import stuff.main
+import cv
+
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+  print("meg eww")
+  
+bot.run("NjYyNzg1Mzc2MTU0NDE5MjEy.XhDbBA.MujjtarDz4DFqoUeWx7xZjo1l6I")
