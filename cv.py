272a0a34a96078f919446b841ecdea1e7d53f83f
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-01-11 12:04:40 +0200

    forgot bot

--- cv.py
+++ cv.py
@@ -2,5 +2,6 @@ import discord
 from discord import Message, Guild, Member
 from typing import Optional
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 bot = commands.Bot(command_prefix="*", case_insensitive=True)
