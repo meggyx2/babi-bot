d10b3568486b92330490fed05c6db46016023344
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-01-11 12:07:33 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import discord
 from discord import Message, Guild, Member
 from typing import Optional
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 import os
 import sys
