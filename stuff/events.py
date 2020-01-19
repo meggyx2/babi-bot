f074c695d38e05b8bfe56f8822a10b98bda42246
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-01-19 15:08:30 +0200

    Update events.py

--- stuff/events.py
+++ stuff/events.py
@@ -21,7 +21,8 @@ async def on_ready():
     assets1 = {large_image:"https://cdn.discordapp.com/attachments/631921445987156021/668410758647775262/141.jpg", large_text:"BOOST BABI"}
     assets2 = {large_Image:"https://cdn.discordapp.com/avatars/660658512052879401/2f907124939a132235eb342d4978465f.webp?size=1024", large_text:"BOT BY $HIKI"}
     stream1 = discord.Streaming(assets=assets1, name="BOOST BABI", details="Boost Babi for cool perks.", url="discord.gg/babi", game="Boost Babi")
-    stream2 = discord.Streaming(assets=assets2, name="BOT BY $HIKI", details="Bot made for the Babi Discord server.", url="https://discord.gg/Qqzy2ds", game="Bot made for the Babi Discord server.")    while True:
+    stream2 = discord.Streaming(assets=assets2, name="BOT BY $HIKI", details="Bot made for the Babi Discord server.", url="https://discord.gg/Qqzy2ds", game="Bot made for the Babi Discord server.")
+    while True:
         await bot.change_presence(activity=stream1)
         await asyncio.sleep(20)
         await bot.change_presence(activity=stream2)
